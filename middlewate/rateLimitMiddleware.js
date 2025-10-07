70975d6d5970a1d794314c07fe6daff9227f8ff3
catlikesice <omarkkillington@proton.me>
2025-10-07 09:18:43 +0200

    Delete middlewate directory

--- middlewate/rateLimitMiddleware.js
+++
@@ -1,9 +0,0 @@
-const rateLimit = require('express-rate-limit');
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100,
-  message: 'Too many requests from this IP, please try again later.',
-});
-
-module.exports = limiter;
